3183d99486b999712fa455a412c6660e9fd41bdf
helbitz <60915274+helbitz@users.noreply.github.com>
2020-08-30 17:04:54 +0300

    ohlord

--- backend/src/mongoose.js
+++ backend/src/mongoose.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 module.exports = function (app) {
-  mongoose.connect(
-    app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
-  ).catch(err => {
+  mongoose.connect("mongodb+srv://helbitz:eran6008@contacts-cluster.sd2ld.mongodb.net/contactsdb?retryWrites=true&w=majority").catch(err => {
     logger.error(err);
     process.exit(1);
   });
